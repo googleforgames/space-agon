9618852c892f89853c5550a3084cc785f398a295
Kent Iso <kentaiso@google.com>
2023-04-24 21:43:37 +0900

    Add header lincense

--- frontend/frontend_test.go
+++ frontend/frontend_test.go
@@ -1,3 +1,17 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -60,7 +74,7 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatalf("Set FRONTEND_ADDR env failed: %v", err)
 	}
-	
+
 	m.Run()
 }
 
